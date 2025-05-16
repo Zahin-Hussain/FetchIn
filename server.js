8c992896e7056a52894abb7d0986cf573d97fa2a
Zahin-Hussain <zahinlinux@gmail.com>
2025-05-16 22:29:48 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const scrapeProfile = require('./fetcher'); // your scraper file
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.get('/takess', async (req, res) => {
   const profileUrl = req.query.url;
